95bda5f0ff6e560f4bc768befd32770e9b0a239e
Nick Patavalis <npat@efault.net>
2015-10-17 04:39:25 +0300

    Added "browse.sh"

---
+++ browse.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# browse.sh
+#
+# Open file, url, or stdin on browser window.
+#
+# e.g. browse myfile.html
+#      browse http://site.com
+#      cat myfile.html | browse 
+#
+
+if [ $# -lt 1 ]; then
+    tmp=`mktemp --tmpdir browse.XXXXXXXXXX`
+    cat > "$tmp"
+    url="file://$tmp"
+elif echo "$1" | grep -q '^[[:alnum:]]\+://'; then
+    url="$1"
+else
+    fp=`readlink -f "$1"`
+    url="file://$fp"
+fi
+
+if [ -n "$BROWSER" ]; then
+    browser="$browser"
+else
+    browser="sensible-browser"
+fi
+
+exec "$browser" "$url"
